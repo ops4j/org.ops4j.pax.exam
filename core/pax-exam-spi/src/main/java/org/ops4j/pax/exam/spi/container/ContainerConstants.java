59fae2e12386b68b4fb2ee84d631752c209934bb
Oliver Lietz <info@oliverlietz.de>
2024-11-27 21:09:52 +0100

    remove obsolete container constants

--- core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/ContainerConstants.java
+++ core/pax-exam-spi/src/main/java/org/ops4j/pax/exam/spi/container/ContainerConstants.java
@@ -19,22 +19,11 @@ package org.ops4j.pax.exam.spi.container;
 
 public class ContainerConstants {
 
-    public static final String BEAN_MANAGER_TYPE = "javax.enterprise.inject.spi.BeanManager";
-    public static final String BEAN_MANAGER_NAME = "BeanManager";
-
-    public static final String WELD_MANAGER_FACTORY = "org.jboss.weld.resources.ManagerObjectFactory";
-    public static final String WELD_SERVLET_LISTENER = "org.jboss.weld.environment.servlet.Listener";
-
-    public static final String OWB_MANAGER_FACTORY = "org.apache.webbeans.container.ManagerObjectFactory";
-    public static final String OWB_SERVLET_LISTENER = "org.apache.webbeans.servlet.WebBeansConfigurationListener";
-
-    public static final String CDI_SERVLET_CLASS = "org.ops4j.pax.exam.servlet.TestRunnerServlet";
     public static final String EXAM_APPLICATION_NAME = "Pax-Exam-Probe";
     public static final String EXAM_CONTEXT_ROOT = "/" + EXAM_APPLICATION_NAME;
     public static final String TESTRUNNER_NAME = "testrunner";
     public static final String TESTRUNNER_URL = "/" + TESTRUNNER_NAME;
     public static final String HTTP_PORT_DEFAULT = "8080";
-    
 
     /** Hidden utility class constructor. */
     private ContainerConstants() {
